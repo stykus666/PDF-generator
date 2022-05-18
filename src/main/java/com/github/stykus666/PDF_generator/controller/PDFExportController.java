ff1651f0cbca68335d3f29fa73cd86379c0a42bf
Slawomir Styczen <stykus@gmail.com>
2022-05-18 20:04:26 +0200

    Updated PDFExportController with: - constructor dependency injection of PDFGeneratorService - annotation @getMapping with response method

--- src/main/java/com/github/stykus666/PDF_generator/controller/PDFExportController.java
+++ src/main/java/com/github/stykus666/PDF_generator/controller/PDFExportController.java
@@ -1,10 +1,35 @@
 package com.github.stykus666.PDF_generator.controller;
 
+import com.github.stykus666.PDF_generator.service.PDFGeneratorService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @Controller
 public class PDFExportController {
 
+private final PDFGeneratorService pdfGeneratorService;
 
+    public PDFExportController(PDFGeneratorService pdfGeneratorService) {
+        this.pdfGeneratorService = pdfGeneratorService;
+    }
+
+    @GetMapping("/pdf/generate")
+    public void generatePDF(HttpServletResponse response) throws IOException {
+        response.setContentType("application/pdf");
+        DateFormat dateFormatter = new SimpleDateFormat("yyyy-MM-dd:hh:mm:ss");
+        String currentDateTime = dateFormatter.format(new Date());
+
+        String headerKey = "Content-Disposition";
+        String headerValue = "attachment; filename=pdf_" + currentDateTime + ".pdf";
+        response.setHeader(headerKey, headerValue);
+
+        this.pdfGeneratorService.export(response);
+    }
 
 }
